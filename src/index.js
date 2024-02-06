c22cdd9ea100d60e8d57789170cb4e1943d9a50e
JayChiang17 <chiang.mengchieh@gmail.com>
2024-02-06 02:20:38 -0800

    change to HashRouter import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // React 18 的写法
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      {/* 使用 BrowserRouter 包裹整个应用 */}
       <App />
     </HashRouter>
   </React.StrictMode>
