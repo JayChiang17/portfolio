f24c0171113f5ea0378ba0e02feabd2d0dcda6a3
JayChiang17 <chiang.mengchieh@gmail.com>
2024-04-14 11:15:54 -0700

    update popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -11,7 +11,7 @@ const Popup = ({ onClose }) => {
         <p>
           This photo of me was created using generative AI technology.&#x1F60A;
         </p>
-        <p>I hpoe you like it</p>
+        <p>I hope you like it</p>
       </div>
     </div>
   );
