2988d6b39e79e29ca80eec910f1f72a09e3bb0f5
JayChiang17 <chiang.mengchieh@gmail.com>
2024-06-13 22:38:22 -0700

    update Nav.js

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -4,20 +4,20 @@ import "../style/MainNav.css"; // 确保 CSS 文件路径正确
 
 const MainNav = () => {
   return (
-    <header className="main-header">
-      <nav className="main-nav">
-        <button className="button">
-          <NavLink smooth to="#aboutMe" activeClassName="active-link">
+    <header className="navHeader">
+      <nav className="nav">
+        <button className="navButton">
+          <NavLink smooth to="#aboutMe" activeClassName="activeLink">
             ABOUTME
           </NavLink>
         </button>
-        <button className="button">
-          <NavLink smooth to="#resume" activeClassName="active-link">
+        <button className="navButton">
+          <NavLink smooth to="#resume" activeClassName="activeLink">
             RESUME
           </NavLink>
         </button>
-        <button className="button">
-          <NavLink smooth to="#contactme" activeClassName="active-link">
+        <button className="navButton">
+          <NavLink smooth to="#contactme" activeClassName="activeLink">
             CONTACT
           </NavLink>
         </button>
