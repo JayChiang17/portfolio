46bb016a3a772223da7d023e8159e1057643334d
Jay Chiang <chiang.mengchieh@gmail.com>
2025-11-15 09:40:53 -0800

    Fix: Remove unused Router import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// 导入 React 库和 Router 相关组件
+// 导入 React 库
 import React, { Suspense } from "react";
-import { HashRouter as Router } from "react-router-dom";
 import "../src/style/App.css";
 import HomePage from "../src/components/HomePage";
 import ErrorBoundary from "../src/components/ErrorBoundary";
